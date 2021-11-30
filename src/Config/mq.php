5351660f4f86964f80fe37810a3ee4a1daec8565
zhen <1149243351@qq.com>
2021-11-30 19:08:52 +0800

    代码合并

--- src/Config/mq.php
+++ src/Config/mq.php
@@ -3,16 +3,23 @@
  * 消息队列相关配置
  */
 return [
-    'mq_type' => 'RocketMQ', // 队列类型（目前只支持 RocketMQ）。 RocketMQ、RabbitMQ、Kafka
+    'mq_type' => 'RocketMQ', // 队列类型（目前只支持 RocketMQ）。 RocketMQ、RabbitMQ、Kafka、Redis
     'reproduce_max_num' => 5, // 最大重新投递次数
     'reproduce_time' => 600, // 重新投递的时间（相当于更新时间）
 
     /**
-     * rocketmq 相关配置
+     * rocketmq 相关配置。队列关键参数：
+     * instance_id => topic => message tag
+     *
+     * Group ID
+     *
+     * message tag：消息标签
+     *
+     * msg key：消息标识（自动生成）
      */
     'rocketmq' => [
         // 默认分组
-        'default' => 'push',
+        'default' => 'default',
         /*
         |--------------------------------------------------------------------------
         | Default RocketMQ Connection Config
@@ -23,11 +30,11 @@ return [
         'access_key' => env('ROCKETMQ_ACCESS_KEY'),
         'secret_key' => env('ROCKETMQ_SECRET_KEY'),
         'group' => [ // 分组
-//        '分组名称' => [
-//            'topic' => 'topic',
-//            'instance_id' => '实例id',
-//            'group_id' => '分组id',
-//        ],
+            'clue' => [
+                'instance_id' => '实例id',
+                'topic' => 'topic',
+                'msg_tag' => '消息标签',
+            ],
         ],
 
         // 路由配置。 msg_tag => 处理类
